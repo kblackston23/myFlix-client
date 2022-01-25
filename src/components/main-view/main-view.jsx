c875bc925ee6bd54be40e0d26c1863d496022b04
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-01-24 19:39:25 -0600

    Added MainView

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainView extends React.Component {
+  render(){
+    return (<div className="main-view">
+      <div>Silence of the Lambs</div>
+      <div>Ponyo</div>
+      <div>The Wind Rises</div>
+    </div>
+    );
+  }
+}
+
+export default MainView;
