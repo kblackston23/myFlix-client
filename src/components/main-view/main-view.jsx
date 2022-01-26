e0663b66200c7b306d7183e6246588ce4cef990f
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-01-25 21:26:57 -0600

    Added movie card and movie view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
+import { MovieCard } from '../movie-card/movie-card';
+
+import { MovieView } from '../movie-view/movie-view';
+
 class MainView extends React.Component {
-  render(){
-    return (<div className="main-view">
-      <div>Silence of the Lambs</div>
-      <div>Ponyo</div>
-      <div>The Wind Rises</div>
-    </div>
-    );
+  constructor(){
+    super();
+    this.state = {
+      movies: [
+        {_id: 1, Title: 'Silence of the Lambs', Description: 'Desc 1', ImagePath: 'img.png' },
+        {_id: 2, Title: 'Ponyo', Description: 'Desc 2', ImagePath: 'img2.png'},
+        {_id: 3, Title: 'The Wind Rises', Description: 'Desc 3', ImagePath: 'img3.png'}
+      ],
+      selectedMovie: null
+    };
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+  
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
+  
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  
+    return (
+      <div className="main-view">
+        {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
+      </div>
+      );
+    }
   }
 }
 
