4aed25416bf871d03f4f58208ad083c3219bbb9c
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-01-27 21:35:15 -0600

    added event listener

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 
@@ -8,14 +9,22 @@ class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-        {_id: 1, Title: 'Silence of the Lambs', Description: 'Desc 1', ImagePath: 'img.png' },
-        {_id: 2, Title: 'Ponyo', Description: 'Desc 2', ImagePath: 'img2.png'},
-        {_id: 3, Title: 'The Wind Rises', Description: 'Desc 3', ImagePath: 'img3.png'}
-      ],
+      movies: [],
       selectedMovie: null
     }
   }
+
+componentDidMount(){
+  axios.get('https://movies-api23.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
   setSelectedMovie(newSelectedMovie) {
     this.setState({
@@ -26,14 +35,14 @@ class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
   
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
   
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
           ))
         }
       </div>
