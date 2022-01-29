d80dcdc50a5852fefa2373688bed94f92e0d9725
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-01-29 15:08:33 -0600

    adding scss files and login view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
+
 import { MovieCard } from '../movie-card/movie-card';
 
 import { MovieView } from '../movie-view/movie-view';
@@ -10,30 +12,39 @@ class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
-    }
+      selectedMovie: null,
+      user: null
+    };
   }
 
-componentDidMount(){
-  axios.get('https://movies-api23.herokuapp.com/movies')
-    .then(response => {
-      this.setState({
-        movies: response.data
-      });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+  componentDidMount(){
+    axios.get('https://movies-api23.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+       console.log(error);
+     });
+  }
 
-  setSelectedMovie(newSelectedMovie) {
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
+    });
+  }
+
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
+
+    if(!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
   
     if (movies.length === 0) return <div className="main-view" />;
   
