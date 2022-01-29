715a8f5b1c1107368b8332f49d6148443ec72d8b
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-01-29 15:58:34 -0600

    update registration view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,9 +3,13 @@ import axios from 'axios';
 
 import { LoginView } from '../login-view/login-view';
 
+import { RegistrationView } from '../registration-view/registration-view'
+
 import { MovieCard } from '../movie-card/movie-card';
 
 import { MovieView } from '../movie-view/movie-view';
+
+import '.main-view.scss'
 
 class MainView extends React.Component {
   constructor(){
@@ -13,7 +17,8 @@ class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
-      user: null
+      user: null,
+      registered: true
     };
   }
 
@@ -29,9 +34,9 @@ class MainView extends React.Component {
      });
   }
 
-  setSelectedMovie(movie) {
+  setSelectedMovie(newSelectedMovie) {
     this.setState({
-      selectedMovie: movie
+      selectedMovie: newSelectedMovie
     });
   }
 
@@ -41,8 +46,17 @@ class MainView extends React.Component {
     });
   }
 
+  onRegister(registered, user) {
+    this.setState({
+      registered,
+      user
+    });
+  }
+
   render() {
     const { movies, selectedMovie, user } = this.state;
+
+    if (!registered) return <RegistrationView onRegister={(registered, username) => this.onRegister(registered, username)} />;
 
     if(!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
   
