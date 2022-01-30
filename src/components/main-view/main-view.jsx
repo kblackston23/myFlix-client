b060cd9829f2d3b160ef794cb205b7fe6cc42eef
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-01-29 21:57:36 -0600

    link error

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,7 +9,7 @@ import { MovieCard } from '../movie-card/movie-card';
 
 import { MovieView } from '../movie-view/movie-view';
 
-import '.main-view.scss'
+import './main-view.scss'
 
 class MainView extends React.Component {
   constructor(){
